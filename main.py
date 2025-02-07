c14c73672cea8bb23544a5d1372a69df0eca8f59
Paulo Piva <cremascopaulo@gmail.com>
2025-02-07 15:28:22 -0300

    DataFrame reading

---
+++ main.py
@@ -0,0 +1,25 @@
+#general
+import io
+
+# data
+import numpy as np
+import pandas as pd
+
+# machine learning
+import keras
+
+# data visualization
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import seaborn as sns
+
+#dataset load
+chicago_taxi_dataset = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/chicago_taxi_train.csv")
+
+# Updates dataframe to use specific columns.
+training_df = chicago_taxi_dataset[['TRIP_MILES', 'TRIP_SECONDS', 'FARE', 'COMPANY', 'PAYMENT_TYPE', 'TIP_RATE']]
+
+print('Read dataset completed successfully.')
+print('Total number of rows: {0}\n\n'.format(len(training_df.index)))
+training_df.head(200)
