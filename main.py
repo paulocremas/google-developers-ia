eacda8d9fb18393fb9a121e80827681ea8647481
Paulo Piva <cremascopaulo@gmail.com>
2025-02-07 15:44:19 -0300

    View dataset statistics

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ chicago_taxi_dataset = pd.read_csv("https://download.mlcc.google.com/mledu-datas
 # Updates dataframe to use specific columns.
 training_df = chicago_taxi_dataset[['TRIP_MILES', 'TRIP_SECONDS', 'FARE', 'COMPANY', 'PAYMENT_TYPE', 'TIP_RATE']]
 
-print('Read dataset completed successfully.')
+#print('Read dataset completed successfully.')
+#print('Total number of rows: {0}\n\n'.format(len(training_df.index)))
+#training_df.head(200)
+
 print('Total number of rows: {0}\n\n'.format(len(training_df.index)))
-training_df.head(200)
+training_df.describe(include='all')
